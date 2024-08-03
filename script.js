662fb342dcf546864397dfb3a1dec188486d2b39
edy <eduardosamsung2010@gmail.com>
2024-08-03 12:32:32 +0300

    create project

---
+++ script.js
@@ -0,0 +1,9 @@
+const startBtn = document.querySelector(".start-btn");
+const startLayout = document.querySelector(".start-layout");
+const car = document.querySelector(".car");
+
+const textArray = ["text1", "text2", "text3", "text4", "text5"];
+
+startBtn.addEventListener("click", () => {
+  startLayout.style.display = "none";
+});
